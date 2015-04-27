6f6affe47ef5c7918c2c38cb8e1c8facefbfd9d6
Vincent Robert <vincent.robert@nanego.com>
2015-04-27 18:35:45 +0200

    Specify http methods in match routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 RedmineApp::Application.routes.draw do
-  match 'auth/failure', :to => 'account#login_with_cas_failure'
-  match 'auth/:provider/callback', :to => 'account#login_with_cas_callback'
-  match 'auth/:provider', :to => 'account#login_with_cas_redirect'
+  match 'auth/failure', :controller => 'account', :action => 'login_with_cas_failure', via: [:get, :post]
+  match 'auth/:provider/callback', :controller => 'account', :action => 'login_with_cas_callback', via: [:get, :post]
+  match 'auth/:provider', :controller => 'account', :action => 'login_with_cas_redirect', via: [:get, :post]
 end
