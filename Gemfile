f1a4a2a026bb0633b2af9152ff8543eac7e57de6
Vincent Robert <vincent.robert@nanego.com>
2019-01-09 14:47:40 +0100

    Improve compatibility with Redmine 4 & Rails 5

--- Gemfile
+++ Gemfile
@@ -1,2 +1,2 @@
-gem 'omniauth', '>= 1.1.1'
-gem 'omniauth-cas', '= 1.0.1'
+gem 'omniauth', '~> 1.8'
+gem 'omniauth-cas', '= 1.1.1'
--- spec/controllers/account_controller_patch_spec.rb
+++ spec/controllers/account_controller_patch_spec.rb
@@ -19,28 +19,28 @@ describe AccountController, type: :controller do
       #in my setup which causes the back_url to be invalid. Let's try to be smart about
       #this directly in the plugin 
       Setting["plugin_redmine_omniauth_cas"]["cas_server"] = "blah"
-      get :login, :back_url => "https%3A%2F%2Fblah%2F"
+      get :login, params: {:back_url => "https%3A%2F%2Fblah%2F"}
       assert_select '#cas-login > a[href=?]', '/auth/cas?origin=https%3A%2F%2Fblah%2F'
     end
   end
 
   context "GET login_with_cas_callback" do
     it "should redirect to /my/page after successful login" do
-      request.env["omniauth.auth"] = {"uid"=>"admin"}
-      get :login_with_cas_callback, :provider => "cas"
+      request.env["omniauth.auth"] = {"uid" => "admin"}
+      get :login_with_cas_callback, params: {:provider => "cas"}
       expect(response).to redirect_to('/my/page')
     end
 
     it "should redirect to /login after failed login" do
-      request.env["omniauth.auth"] = {"uid"=>"non-existent"}
+      request.env["omniauth.auth"] = {"uid" => "non-existent"}
       Setting["plugin_redmine_omniauth_cas"]["cas_server"] = "http://cas.server/"
-      get :login_with_cas_callback, :provider => "cas"
+      get :login_with_cas_callback, params: {:provider => "cas"}
       expect(response).to redirect_to('/login')
     end
 
     it "should set a boolean in session to keep track of login" do
-      request.env["omniauth.auth"] = {"uid"=>"admin"}
-      get :login_with_cas_callback, :provider => "cas"
+      request.env["omniauth.auth"] = {"uid" => "admin"}
+      get :login_with_cas_callback, params: {:provider => "cas"}
       expect(response).to redirect_to('/my/page')
       assert session[:logged_in_with_cas]
     end
@@ -52,7 +52,7 @@ describe AccountController, type: :controller do
 
     it "should redirect to CAS logout if previously logged in with CAS" do
       session[:logged_in_with_cas] = true
-      Setting["plugin_redmine_omniauth_cas"]["cas_server"] = "http://cas.server/"
+      Setting["plugin_redmine_omniauth_cas"]["cas_server"] = "http://cas.server"
       get :logout
       expect(response).to redirect_to('http://cas.server/logout?gateway=1&service=http://test.host/')
     end
